db1c1059bb1207ce5c2bb30b2c48b0893fb23d4f
Ahmed Khalaf <pxlshpr@gmail.com>
2022-10-23 08:40:25 +0500

    tag increment

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["PrepNetworkController"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/pxlshpr/PrepUnits", from: "0.0.112"),
+        .package(url: "https://github.com/pxlshpr/PrepUnits", from: "0.0.114"),
         .package(url: "https://github.com/pxlshpr/SwiftSugar", from: "0.0.69"),
     ],
     targets: [
