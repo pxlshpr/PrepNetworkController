ab5118dd57a419e0a9e1efea512710969993b3d2
Ahmed Khalaf <pxlshpr@gmail.com>
2022-10-23 09:03:26 +0500

    tag increment

--- Sources/PrepNetworkController/NetworkController.swift
+++ Sources/PrepNetworkController/NetworkController.swift
@@ -64,17 +64,6 @@ public class NetworkController {
         request.httpBody = httpBody as Data
         return request
     }
-    
-    public func searchFoods(with string: String, page: Int = 1, per: Int = 25) async throws -> FoodsPage {
-        let urlString = "\(baseUrlString)/foods/?page=\(page)&per=\(per)"
-        guard let url = URL(string: urlString) else {
-            throw NetworkControllerError.badUrl
-        }
-        
-        let (data, _) = try await URLSession.shared.data(from: url)
-        let decoder = JSONDecoder()
-        return try decoder.decode(FoodsPage.self, from: data)
-    }
 }
 
 public struct PageMetadata: Codable {
